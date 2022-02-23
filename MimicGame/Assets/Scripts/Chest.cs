225e7540308c9fcb641eba33ed695f3acdb6c0e2
Kyle Coulter <kcoulter@icomponent.com>
2022-02-22 23:51:37 -0500

    Created Chest.cs.

---
+++ MimicGame/Assets/Scripts/Chest.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+
+namespace UnityTemplateProjects
+{
+    public class Chest : MonoBehaviour
+    {
+        public Material trimMaterial;
+        public Material chestMaterial;
+        public Material lockMaterial;
+        public String owner;
+        public Boolean hasRune;
+    }
+}
