a45860ca3179e23e441efbf7482302faa38865af
Kyle Coulter <kcoulter@icomponent.com>
2022-02-26 17:51:03 -0500

    Added hint text to phone prefab. Modified animations for OpenPhone method.

--- MimicGame/Assets/Scripts/PhoneManager.cs
+++ MimicGame/Assets/Scripts/PhoneManager.cs
@@ -13,6 +13,7 @@ namespace UnityTemplateProjects
         [SerializeField] private Transform closedPhoneLocation;
         [SerializeField] private TextMeshPro payment;
         [SerializeField] private TextMeshPro reward;
+        [SerializeField] private TextMeshPro hint;
 
         private Boolean isPhoneOpen;
         private Vector3 openPhone;
@@ -28,12 +29,19 @@ namespace UnityTemplateProjects
         public void OpenPhone()
         {
             var seq = DOTween.Sequence();
+            seq.Insert(0f, hint.DOFade(0, 0f));
+            seq.Insert(0f, payment.DOFade(0, 0f));
+            seq.Insert(0f, reward.DOFade(0, 0f));
             seq.Insert(0f, phoneTransform.DOLocalMove(openPhone, 1f));
             
             seq.InsertCallback(.2f, delegate
             {
                 isPhoneOpen = true;
             });
+
+            seq.Insert(1f, payment.DOFade(1, 2f));
+            seq.Insert(2f, reward.DOFade(1, 2f));
+            seq.Insert(5f, hint.DOFade(1, 4f));
         }
 
         public void ClosePhone()
@@ -45,6 +53,7 @@ namespace UnityTemplateProjects
                 isPhoneOpen = false;
             });
             seq.Insert(3f, phoneTransform.DOLocalMove(closedPhone, 1f));
+            seq.Insert(4f, hint.DOFade(0, 0f));
         }
 
         public bool IsPhoneOpen()
